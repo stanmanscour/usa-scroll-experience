bfdd7626f5a41b74fe7b36058b89fe49127fe293
stanmanscour <stanislas.manscour@hetic.net>
2017-01-14 17:41:34 +0100

    init width & height

--- src/js/01-init.js
+++ src/js/01-init.js
@@ -1,3 +1,28 @@
 // code pour l'initialisation
 // # calcul taille fenetre 
 // # loading
+
+var windowWidth,
+windowHeight;
+
+var updateWindow = function(){
+	windowHeight = window.innerHeight;
+	windowWidth = window.innerWidth;
+	
+	var $entranceContainer = $('.usa-entrance-container');
+
+	$entranceContainer.css('width', windowWidth+'px');
+	$entranceContainer.css('height', windowHeight+'px');
+	
+	console.log("nouvelle height = " +windowHeight+"px");
+	console.log("nouvelle width = " +windowWidth+"px");
+}
+
+
+$(document).ready(function(){
+	updateWindow();
+	
+	$(window).resize(function(){
+		updateWindow();
+	})
+})
