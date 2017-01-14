d96e14caad523f45f4c9655780a244c261bb9d23
stanmanscour <stanislas.manscour@hetic.net>
2017-01-14 15:39:41 +0100

    init files

---
+++ dist/js/script.js
@@ -0,0 +1,26 @@
+console.log('hey');
+$(function(){
+
+  var controller = new ScrollMagic.Controller({vertical: false});
+
+  //mon animation que je vais linker plus tard
+  var animation = TweenMax.to(".monTexte", 1, {
+    rotation:360, y:100
+  }, 0.5)
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#scene",
+    duration: 300 // 300px
+  })
+
+  .on('start', function(){
+    // console.log("c'est le début")
+  })
+  .on('end', function(event){
+    // console.log("c'est la fin")
+  })
+  .setTween(animation) // link l'animation
+  .addTo(controller) // link le controller
+  .addIndicators(); // ajoute les indicateurs pour noobs
+
+  });
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    sourcemap = require('gulp-sourcemaps'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['styles', 'scripts'], function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            https: true
+        },
+        port: 3000
+    });
+
+    gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch('src/js/*.js', ['scripts']);
+  	gulp.watch('*.html').on('change', browserSync.reload);
+  	gulp.watch('src/scss/**/*.scss').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+      return gulp.src('src/scss/style.scss')
+        .pipe(sourcemap.init())
+        .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cleanCSS())
+        .pipe(sourcemap.write())
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('images', function(){
+    return gulp.src('src/img/*')
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest('dist/img/'));
+})
+
+gulp.task('assets', function(){
+    return gulp.src([
+            'assets/js/*.js'
+        ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('scripts', function() {
+  return gulp.src([
+        'src/js/*.js'
+    ])
+    .pipe(concat('script.js'))
+    //.pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('default', ['serve'], function() {
+
+});
