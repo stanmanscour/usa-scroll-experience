84284acc3fc04f3a483b664921d341fd234bdd53
stanmanscour <stanislas.manscour@hetic.net>
2017-02-02 23:46:53 +0100

    testing parallax

--- src/js/02-entrance.js
+++ src/js/02-entrance.js
@@ -1,15 +1,14 @@
 
 $(function () { // wait for document ready
-  
+
   var controller = new ScrollMagic.Controller();
 
   var horizontalSlide = new TimelineMax()
   // animate panels
-  .to("#js-slideContainer", 1,   {x: "-20%"})	
+  .to("#js-slideContainer", 1,   {x: "-20%"}) 
   .to("#js-slideContainer", 1,   {x: "-40%"})
   .to("#js-slideContainer", 1,   {x: "-60%"})
   .to("#js-slideContainer", 1,   {x: "-80%"})
-
 
   // create scene to pin and link animation
   new ScrollMagic.Scene({
@@ -24,45 +23,21 @@ $(function () { // wait for document ready
 
   });
 
-function disableScroll() {
- window.onwheel = preventDefault;
-}
-
 var toggled = false;
 var locked = false;
 
-$('#button-julien').on('click', function(){
-    // var sectionPos = $('');
-    // var currentPos = $('.section-two').offset().left;
-    var scrollPosition = [
-    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-    self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-    ];
-    
+
+var sectionDeuxOpen = false;
+
+
+
+$('.js-open-sectionDeux').on('click', function(){
+
+  if (!sectionDeuxOpen){ /* si la section 2 est fermée */
+
     $('html, body').stop().animate({
-     scrollTop: 670
-   }, 1000, function(){
-    if (locked){
-     $('body').on({'mousewheel': function(e) {
-          //if (e.target.id == 'el') return;
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      }) 
-      locked = true;
-   } else {
-          $('body').off({'mousewheel': function(e) {
-          //if (e.target.id == 'el') return;
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      }) 
-     locked = false;
-   }
- });
-    
-    
-    if (toggled === false){
+      scrollTop: 3605 * 20 / 100
+    }, 1000, function(){
       $('#section-2-id').css({
         'display': 'block',
         'height': '100%',
@@ -73,18 +48,48 @@ $('#button-julien').on('click', function(){
         'left': '50%',
         'transform': 'translate(-50%, 25%)'
       })
-      $(document).css('overflow-x', '')
-      toggled = true;
-    } else {
-      toggled = false;
-      $('#section-2-id').css({
-        'display': 'none',
-        'background': 'blue'
-      })
-    }
+    });
 
+    sectionDeuxOpen = true;
+
+  } else { /* si la section 2 est ouverte */
+
+   $('#section-2-id').css({
+    'display': 'none',
+    'background': 'blue'
   })
 
+   sectionDeuxOpen = false;
+
+ }
+})
 
 
-// window.scrollTo()
+
+    //var text1 =$('.section_discover_text1').parent();
+    var text1 = $('#text1');
+    var text2 = $('#text2');
+    var text3 = $('#text3');
+    
+
+
+    // var Man = document.getElementById('element2');
+    text1.homePos = { x: parseInt(text1.css('left').split('px')[0]), y: parseInt(text1.css('top').split('px')[0]) };
+    text2.homePos = { x: parseInt(text1.css('left').split('px')[0]), y: parseInt(text1.css('top').split('px')[0]) };
+    text3.homePos = { x: parseInt(text1.css('left').split('px')[0]), y: parseInt(text1.css('top').split('px')[0]) };
+    // Man.homePos = { x: Man.offsetLeft, y: Man.offsetTop };
+    console.log(text1.homePos);
+    $('.section-two').mousemove(function (e) {
+        parallax(e, text1, 10);
+        parallax(e, text2, 25);
+        parallax(e, text3, 20);
+    });
+
+function parallax(e, target, layer) {
+    var x = target.homePos.x - (e.pageX - target.homePos.x) / layer;
+    var y = target.homePos.y - (e.pageY - target.homePos.y) / layer;
+    //$(target).offset({ top: y ,left : x });
+    $(target).css('left', x+'px')
+    $(target).css('top', y+'px')
+    console.log(x, y);
+};
