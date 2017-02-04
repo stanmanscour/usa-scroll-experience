f6d32b8b4d984cd658d76b2869652508e6e96ac4
stanmanscour <stanislas.manscour@hetic.net>
2017-02-04 20:44:03 +0100

    animation 2nd section

--- dist/js/script.js
+++ dist/js/script.js
@@ -73,9 +73,9 @@ var section2Taille = $section_two.width();
 var tween = new TimelineMax ()
 .add([
         // TweenMax.to(".section-two .section_bcgImage_flag>img", 1, {left: "-30"}),
-        TweenMax.to(".section_two>.content .section_mainImage>img", 1, {left: '20%'}),
+        TweenMax.to(".section_two>.content .section_mainImage>img", 1, {left: '15%'}),
         TweenMax.to(".section_two>.content .section_bcgImage_1", 1, {left: '70%'}),
-        TweenMax.to(".section_two>.content .section_title", 1, {left: '55%'}),
+        TweenMax.to(".section_two>.content .section_title", 1, {left: '50%'}),
         TweenMax.to(".section_two>.content .section_text", 1, {left: '55%'})
         ]);
 
@@ -102,56 +102,121 @@ function parallax(e, target, layer, a, b) {
     $(target).css(b, y+'px')
 };
 
+var $cat_year = $('.contratATerme_year'),
+    $cat_background = $('.contratATerme_background img'),
+    $cat_action = $('.js-contratATerme_action');
+
 $(function() {
-
-    var $ww_year = $('.whitewater_year'),
-    $ww_clinton_img = $('.whitewater_clinton img'),
-    $ww_title = $('.whitewater_title'),
-    $ww_intro = $('.whitewater_intro'),
-    $ww_opt_clinton = $('.whitewater_optionalClinton'),
-    $ww_background = $('.whitewater_background img'),
-    $ww_action = $('.whitewater_action');
-
-    $ww_year.homePos = { x: $ww_year.css('left').split('px')[0], y: $ww_year.css('top').split('px')[0] };
-    $ww_clinton_img.homePos = { x: $ww_clinton_img.css('left').split('px')[0], y: $ww_clinton_img.css('bottom').split('px')[0] };
-    $ww_title.homePos = { x: $ww_title.css('left').split('px')[0], y: $ww_title.css('top').split('px')[0] };
-    $ww_intro.homePos = { x: $ww_intro.css('left').split('px')[0], y: $ww_intro.css('top').split('px')[0] };
-    $ww_background.homePos = { x: $ww_background.css('left').split('px')[0], y: $ww_background.css('top').split('px')[0] };
-    $ww_action.homePos = { x: $ww_action.css('left').split('px')[0], y: $ww_action.css('top').split('px')[0] };
-
-    $('.section_two').mousemove( function(e){
-        // $ww_year.homePos = { x: $ww_year.css('left').split('px')[0], y: $ww_year.css('top').split('px')[0] };
-        // $ww_clinton_img.homePos = { x: $ww_clinton_img.css('left').split('px')[0], y: $ww_clinton_img.css('bottom').split('px')[0] };
-
-        parallax(e, $ww_background, 40, 'left', 'top');
-        parallax(e, $ww_year, 40, 'left', 'top');
-        // parallax(e, $ww_clinton_img, 25, 'left', 'bottom');
-        // parallax(e, $ww_title, 40, 'left', 'top');
-        // parallax(e, $ww_intro, 60, 'left', 'top');
-        parallax(e, $ww_action, 60, 'left', 'top');
-
-    })
-
-})
-    // text2.homePos = { x: parseInt(text1.css('left').split('px')[0]), y: parseInt(text1.css('top').split('px')[0]) };
-    // text3.homePos = { x: parseInt(text1.css('left').split('px')[0]), y: parseInt(text1.css('top').split('px')[0]) };
-    // Man.homePos = { x: Man.offsetLeft, y: Man.offsetTop };
-    // console.log(text1.homePos);
-    // $('.section-two').mousemove(function (e) {
-    //     parallax(e, text1, 10);
-    //     parallax(e, text2, 25);
-    //     parallax(e, text3, 20);
-    // });
 
     
 
+    $cat_year.homePos = { x: $cat_year.css('left').split('px')[0], y: $cat_year.css('top').split('px')[0] };
+    $cat_background.homePos = { x: $cat_background.css('left').split('px')[0], y: $cat_background.css('top').split('px')[0] };
+    $cat_action.homePos = { x: $cat_action.css('left').split('px')[0], y: $cat_action.css('top').split('px')[0] };
 
+    $('.section_two').mousemove( function(e){
+        // $cat_year.homePos = { x: $cat_year.css('left').split('px')[0], y: $cat_year.css('top').split('px')[0] };
+        // $cat_clinton_img.homePos = { x: $cat_clinton_img.css('left').split('px')[0], y: $cat_clinton_img.css('bottom').split('px')[0] };
+
+        parallax(e, $cat_background, 40, 'left', 'top');
+        parallax(e, $cat_year, 40, 'left', 'top');
+        // parallax(e, $cat_clinton_img, 25, 'left', 'bottom');
+        // parallax(e, $cat_title, 40, 'left', 'top');
+        // parallax(e, $cat_intro, 60, 'left', 'top');
+        parallax(e, $cat_action, 60, 'left', 'top');
+
+    })
+})
 
 /*
 *
-*
+* Mouse Action
 *
 */
+
+var $cat_title = $('.contratATerme_title');
+var $cat_intro = $('.contratATerme_intro');
+var $cat_clinton = $('.contratATerme_clinton img');
+
+var tweenCatClinton = TweenMax.to($cat_clinton, 1, {
+    opacity: "0",
+    bottom: '-19%',
+    left: "10%",
+    ease:Power1.easeInOut
+})
+tweenCatClinton.pause();
+
+var tweenCatTexts = TweenMax.to($cat_intro, 1, {
+    opacity: "0",
+    top: '24%',
+    ease:Power1.easeInOut
+})
+tweenCatTexts.pause();
+
+var tweenCatTitle = TweenMax.to($cat_title, 1, {
+    opacity: "0",
+    top: '20%',
+    ease:Power1.easeInOut,
+
+})
+tweenCatTitle.pause();
+
+$cat_secondContent = $('.section_contentSecond');
+var tweenCatSecondContent = TweenMax.to($cat_secondContent, 2, {
+    bottom: '0%',
+    ease:Power1.easeInOut
+})
+tweenCatSecondContent.pause();
+
+var tl = new TimelineLite();
+tl.add('skew')
+    .add([tweenCatTexts.play(), tweenCatTitle.play(), tweenCatClinton.play()])
+    .add(tweenCatSecondContent.play())
+tl.pause();
+
+
+$(function(){
+
+    var cat_content_open = false;
+    var $cat_content = $('.section_contentSecond');
+
+    $cat_action.click(function(){
+
+        // si c'est fermé -> ouvrir
+        if (!cat_content_open){
+
+
+
+            tl.play();
+            // tweenCatClinton.play();
+            // tweenCatTitle.play();
+            // tweenCatTexts.play();
+
+
+            // animations
+            
+            
+
+            
+            // cacher le menu
+
+            cat_content_open = true;
+
+        // si c'est ouvert -> fermer
+        } else {
+
+           tl.reverse();
+
+            //
+
+            cat_content_open = false;
+        }
+        
+
+    })
+
+
+})
 
 // $(function () { // wait for document ready
 
