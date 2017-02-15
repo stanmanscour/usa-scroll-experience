88d2e64265f26466ffa73bc385a2e46be5675651
stanmanscour <stanislas.manscour@hetic.net>
2017-02-15 23:34:48 +0100

    test functionality scroll with drag

--- src/js/05-mafiatrump.js
+++ src/js/05-mafiatrump.js
@@ -16,3 +16,32 @@ var scene = new ScrollMagic.Scene({ triggerElement: ".section_three .content", d
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller_section3);
 
+    var parallax = function parallax(e, target, layer, a, b) {
+        var x = target.homePos.x - (e.pageX - target.homePos.x) / layer;
+        var y = target.homePos.y - (e.pageY - target.homePos.y) / layer;
+        //$(target).offset({ top: y ,left : x });
+        $(target).css(a, x + 'px')
+        $(target).css(b, y + 'px')
+    };
+
+    var $cat_year = $('.contratATerme_year'),
+        $cat_background = $('.contratATerme_background img'),
+        $cat_action = $('.js-contratATerme_action');
+
+    var parallaxCatActionAuthorised = true;
+
+    $(function() {
+
+        $cat_year.homePos = { x: $cat_year.css('left').split('px')[0], y: $cat_year.css('top').split('px')[0] };
+        $cat_background.homePos = { x: $cat_background.css('left').split('px')[0], y: $cat_background.css('top').split('px')[0] };
+        $cat_action.homePos = { x: $cat_action.css('left').split('px')[0], y: $cat_action.css('top').split('px')[0] };
+
+        $('.section_two').mousemove(function(e) {
+            parallax(e, $cat_background, 40, 'left', 'top');
+            parallax(e, $cat_year, 40, 'left', 'top');
+
+            if (parallaxCatActionAuthorised) {
+                parallax(e, $cat_action, 60, 'left', 'top');
+            }
+        })
+    })
