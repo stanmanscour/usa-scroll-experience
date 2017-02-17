ded9ada441438aec5fba84e0ab99de2b1b358ead
julacoste <ju_lacoste@yahoo.fr>
2017-02-17 11:47:13 +0100

    new img and fix parallax img primary section

--- src/js/04-contratterme.js
+++ src/js/04-contratterme.js
@@ -12,7 +12,7 @@
     var tweenSection2 = new TimelineMax()
         .add([
             // TweenMax.to(".section-two .section_bcgImage_flag>img", 1, {left: "-30"}),
-            TweenMax.to(".section_two>.content .section_mainImage>img", 1, { left: '8%', bottom: '0%' }),
+            TweenMax.to(".section_two>.content .section_mainImage>img", 1, { left: '8%', bottom: '-10%' }),
             TweenMax.to(".section_two>.content .section_bcgImage_1", 1, { left: '50%' }),
             TweenMax.to(".section_two>.content .section_title", 1, { left: '50%' }),
             TweenMax.to(".section_two>.content .section_text", 1, { left: '55%' })
--- src/js/05-mafiatrump.js
+++ src/js/05-mafiatrump.js
@@ -3,7 +3,7 @@ var controller_section3 = new ScrollMagic.Controller({ vertical: false });
 var tweenSection3 = new TimelineMax()
     .add([
         // TweenMax.to(".section-two .section_bcgImage_flag>img", 1, {left: "-30"}),
-        TweenMax.to(".section_three>.content .section_mainImage>img", 1, { left: '3%', bottom: '-10%' }),
+        TweenMax.to(".section_three>.content .section_mainImage>img", 1, { left: '3%', bottom: '-20%' }),
         TweenMax.to(".section_three>.content .section_bcgImage_1", 1, { left: '70%' }),
         TweenMax.to(".section_three>.content .section_title", 1, { left: '47%' }),
         TweenMax.to(".section_three>.content .section_title2", 1, { left: '50%' }),
