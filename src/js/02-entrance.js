43f161f4b59f66b01ca36684325804535f4a41e8
stanmanscour <stanislas.manscour@hetic.net>
2017-02-19 19:53:35 +0100

    animations entrance

--- src/js/02-entrance.js
+++ src/js/02-entrance.js
@@ -23,3 +23,62 @@
   
 //})
 
+$(function(){
+
+	var entranceShowTimeline = new TimelineMax();
+
+	var $entTitle = $('.entrance_title'),
+		$entDesc = $('.entrance_description'),
+		$entHow = $('.entrance_howTo'),
+		$entScrollA = $('.entrance_scrollAction'),
+		$globalLogo = $('.menu-header')
+
+	entranceShowTimeline
+		.to($entTitle, 3, {})
+		.from($entTitle, 1, {
+			top: "30%",
+			opacity: 0,
+			ease: Power1.easeInOut
+		}, "bigTitle")
+		.from($entDesc, 1, {
+			top: "43%",
+			opacity: 0,
+			ease: Power1.easeInOut
+		}, "bigTitle+=1.5")
+		.from($entHow, 1, {
+			opacity: 0,
+			ease: Power1.easeInOut
+		}, 7)
+		.from($entScrollA, 0.4, {
+			opacity: 0
+		}, 9)
+		.from($globalLogo, 0.4, {
+			opacity: 0
+		}, 9)
+
+
+	// animation scroll
+
+	var scrollActionTimeline = new TimelineMax({onComplete:function() {
+    	this.restart()}
+	});
+
+	var $scSpanBlack = $('.entrance_scrollAction .spanBlack');
+	var $scSpanWhite = $('.entrance_scrollAction .spanWhite');
+
+	scrollActionTimeline
+		.to($scSpanBlack, 1, {
+			width: "90px",
+			ease: Power1.easeInOut
+		})
+		.to($scSpanWhite, 1, {
+			width: "90px",
+			ease: Power1.easeInOut
+		})
+		.to($scSpanWhite, 1, {
+
+		})
+		
+
+
+})
