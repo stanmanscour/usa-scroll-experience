b8bcd8b14f59df33f3e1222458fce019acc266c4
julacoste <ju_lacoste@yahoo.fr>
2017-02-20 18:46:31 +0100

    fixed bug main image suite de la retouche fade

--- src/js/06-whitewater.js
+++ src/js/06-whitewater.js
@@ -3,7 +3,7 @@ var controller_section4 = new ScrollMagic.Controller({ vertical: false });
 var tweenSection4 = new TimelineMax()
     .add([
         // TweenMax.to(".section-two .section_bcgImage_flag>img", 1, {left: "-30"}),
-        TweenMax.to(".section_four>.content .section_mainImage>img", 1, { left: '2%', bottom: "-10%" }),
+        TweenMax.to(".section_four>.content .section_mainImage>img", 1, { left: '2%', bottom: "-15%" }),
         TweenMax.to(".section_four>.content .section_bcgImage_1", 1, { left: '7%' }),
         TweenMax.to(".section_four>.content .section_title", 1, { left: '56%' }),
         TweenMax.to(".section_four>.content .section_text", 1, { left: '54%' })
--- src/js/08-emailcontroversy.js
+++ src/js/08-emailcontroversy.js
@@ -3,7 +3,7 @@ var controller_section6 = new ScrollMagic.Controller({ vertical: false });
 var tweenSection6 = new TimelineMax()
     .add([
         // TweenMax.to(".section-two .section_bcgImage_flag>img", 1, {left: "-30"}),
-        TweenMax.to(".section_six>.content .section_mainImage>img", 1, { left: '0.2%', bottom: "-10%" }),
+        TweenMax.to(".section_six>.content .section_mainImage>img", 1, { left: '0.2%', bottom: "-15%" }),
         TweenMax.to(".section_six>.content .section_bcgImage_1", 1, { left: '40%' }),
         TweenMax.to(".section_six>.content .section_title", 1, { left: '45%' }),
         TweenMax.to(".section_six>.content .section_text", 1, { left: '53%' })
